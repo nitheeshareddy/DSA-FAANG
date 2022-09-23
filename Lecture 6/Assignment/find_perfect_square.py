dc525b2f043bcc435e516f77cad67db2296bd483
nitheeshareddy <nitheeshareddy17@gmail.com>
2022-09-23 10:41:27 -0400

    Create find_perfect_square.py

---
+++ Lecture 6/Assignment/find_perfect_square.py
@@ -0,0 +1,19 @@
+#Time complexity: O(logn) - The search space is limited to half so the recurrence relation is T(n/2) + c
+#Space Complexity: O(1) - No extra space is used
+def is_perfect_square(left, right, num):
+    while left <= right:
+        mid = left + (right - left)//2
+        if mid * mid == num:
+            return True
+        elif mid*mid < num:
+            return is_perfect_square(mid+1, right, num)
+        else:
+            return is_perfect_square(left, mid-1, num)
+    return False      
+
+#driver code
+num = 10
+left = 0
+right = num
+result = is_perfect_square(left, right, num)
+print(result)
